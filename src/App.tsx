00a9d89633bd43e5a103d0ca75ff58a1c495f540
eransnir-jpg <eransnir@gmail.com>
2026-03-08 17:27:07 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/devision-attendance>
+      <BrowserRouter basename="/devision-attendance">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
