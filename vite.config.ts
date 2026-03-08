6cc35755b1ae50bbd12a18845daf068326cf6825
eransnir-jpg <eransnir@gmail.com>
2026-03-08 16:26:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/./',
+  base: '/devision-attendance/',
   server: {
     host: "::",
     port: 8080,
